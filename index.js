5dc631c48c8ec914b372b3b28ef5601eb48a9ad4
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-03-25 22:28:19 +0600

    update project

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express');
+
+const cors = require('cors');
+
+const bodyParser=require('body-parser');
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const users =['asad','Moin','Sabed','Sumit','sohana']
 
 
 app.get('/',(req,res)=>{
-    res.send ('I know node !!!!');
+    const fruit={
+        product:'ada',
+        price:220
+
+    }
+    res.send (fruit);
+})
+app.get('/fruits/banana',(req,res)=>{
+    res.send({fruit:'banana',quantity:1000,price:10000});
+})
+app.get('/users/:id',(req,res)=>{
+    const id=req.params.id;
+    const name=users[id];
+    res.send({id,name});
+    console.log(req.query.sort);
+})
+
+//Post
+app.post('/addUser',(req,res)=>{
+    // console.log(req.body);
+    // console.log('data received',req.body);
+    //Save to database
+    const user=req.body;
+    user.id=55;
+    res.send(user);
 })
 
 app.listen(4200, ()=>console.log('Listening to port 4200'));
